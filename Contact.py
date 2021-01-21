b0dd0c52d8abd992be845db01848f07ba6b0befa
vishakha95 <vbhosale25@gmail.com>
2021-01-21 12:44:19 +1100

    adding new methods file and changes to payload making it more readable

--- Contact.py
+++ Contact.py
@@ -1,46 +1,7 @@
-import requests 
-headers = {
-    'content-type': "application/json",
-    'accept': "application/json",
-    'authorization': "Bearer 2uIcSpFAlNluC5FMiBYU7aMipLdFMg9NCJ-K_0nfPBU"
-}
-
-response = requests.get("https://api.sandbox.split.cash/contacts", headers = headers)
-print(response.status_code)
-print(response.text)
-
-#getting a single customer details from id
-print("\nIndividual Customer\n")
-response = requests.get("https://api.sandbox.split.cash/contacts/2e115cc1-1566-41d4-ad66-56814e1c55b6", headers = headers)
-print(response.text)
-
-#getting your bank account id
-print("\nMy Bank ID\n")
-response = requests.get("https://api.sandbox.split.cash/bank_accounts", headers = headers)
-print(response.text)
-
-#making a payment
-print("\nMaking a payment to a contact\n")
-payload = "{\"description\":\"The SuperPackage\",\"matures_at\":\"2021-01-19T00:00:00Z\",\"your_bank_account_id\":\"8cd332a0-8cf3-479b-9e80-c17a22bf63ad\",\"payouts\":[{\"amount\":30,\"description\":\"testing payments via code\",\"recipient_contact_id\":\"2e115cc1-1566-41d4-ad66-56814e1c55b6\",\"metadata\":{\"invoice_ref\":\"BILL-0001\",\"invoice_id\":\"c80a9958-e805-47c0-ac2a-c947d7fd778d\",\"custom_key\":\"Custom string\",\"another_custom_key\":\"Maybe a URL\"}},{\"amount\":30,\"description\":\"A scuba dive SDS5464\",\"recipient_contact_id\":\"2e115cc1-1566-41d4-ad66-56814e1c55b6\"}]}"
-response = requests.post("https://api.sandbox.split.cash/payments", payload, headers = headers)
-print(response.text) 
-
-#create a contact
-print("\Creating a Split contact")
-payload = "{\"name\":\"contactadded with api\",\"email\":\"withapi@gmail.com\",\"branch_code\":\"123056\",\"account_number\":\"099999999\"}"
-response = requests.post("https://api.sandbox.split.cash/contacts/anyone",payload,headers = headers)
-print(response.status_code)
-
-#propose an unassigned agreement
-
-print("\n Propose an agreement\n")
-payload = "{\"expiry_in_seconds\":6000,\"single_use\":false,\"terms\":{\"per_payout\":{\"min_amount\":null,\"max_amount\":10000},\"per_frequency\":{\"days\":7,\"max_amount\":1000000}}}"
-response = requests.post("https://api.sandbox.split.cash/unassigned_agreements", payload, headers = headers)
-print(response.text)
-
-#Direct Debit a contact 
-print("\nDirect Debit a contact\n")
-payload = "{\"description\":\"Direct Debiting a contact via api\",\"matures_at\":\"20121-01-20T02:10:56.000Z\",\"amount\":1000,\"authoriser_contact_id\":\"c4e6d5b7-a6a0-4b68-b481-0caaac0d3875\",\"your_bank_account_id\":\"8cd332a0-8cf3-479b-9e80-c17a22bf63ad\",\"precheck_funds\":false,\"metadata\":{\"custom_key\":\"Custom string\",\"another_custom_key\":\"Maybe a URL\"}}"
-response = requests.post("https://api.sandbox.split.cash/payment_requests",payload, headers = headers)
-print(response.text)
-
+import Methods
+Methods.getcontact()
+Methods.getsinglecontact()
+Methods.makingapayment()
+Methods.createcontact()
+Methods.unassigned_agreements()
+Methods.directdebit()
---
+++ Methods.py
@@ -0,0 +1,94 @@
+import requests
+import json
+headers = {
+    'content-type': "application/json",
+    'accept': "application/json",
+    'authorization': "Bearer 2uIcSpFAlNluC5FMiBYU7aMipLdFMg9NCJ-K_0nfPBU"
+}
+
+def getcontact():
+    response = requests.get(
+        "https://api.sandbox.split.cash/contacts", headers=headers)
+    print(response.status_code)
+    print(response.text)
+
+
+#getting a single customer details from id
+def getsinglecontact():
+    print("\nIndividual Customer\n")
+    url = "https://api.sandbox.split.cash/contacts/2e115cc1-1566-41d4-ad66-56814e1c55b6"
+    response = requests.get(url, headers = headers)
+    print(response.text)
+
+""" 
+#getting your bank account id
+print("\nMy Bank ID\n")
+response = requests.get("https://api.sandbox.split.cash/bank_accounts", headers = headers)
+print(response.text) 
+ """
+#making a payment
+def makingapayment():
+    print("\nMaking a payment to a contact\n")
+    payload = {
+        'description': 'making_api_payment',
+        'matures_at': '2021-01-21T00:00:00Z',
+        'payouts' : [
+            {
+                'amount':300, 
+                'description':'making_api_payment',
+                'recipient_contact_id':'2e115cc1-1566-41d4-ad66-56814e1c55b6'
+            }]
+    }
+    url = "https://api.sandbox.split.cash/payments"
+    response = requests.post(url, json.dumps(payload), headers = headers)
+    print(response.status_code) 
+
+
+#creating a contact
+def createcontact():
+    payload = {
+        'name': 'creating a contactwithapi',
+        'email':'createcontactviaapi@gmail.com',
+        'branch_code':'011101',
+        'account_number':'003333333'
+    }
+    url = "https://api.sandbox.split.cash/contacts/anyone"
+    response = requests.post(url,json.dumps(payload),headers = headers)
+    print(response.status_code)
+
+
+# propose unassigned agreement
+def unassigned_agreements():
+    payload = {
+        'expiry_in_seconds':'900',
+        'terms': 
+            {'per_payout': 
+                {
+                    'min_amount': 'null',
+                    'max_amount': '1000',
+                },
+                'per_frequency': 
+                    {
+                    'days': '7',
+                    'max_amount': '10000'
+                }
+                }
+             
+    }
+    url = "https://api.sandbox.split.cash/unassigned_agreements"
+    response = requests.post(url, json.dumps(payload), headers=headers)
+    print(response.text)
+
+def directdebit():
+    payload = {
+        'description': '21.01 api_payment_request',
+        'matures_at': '2021-01-21T02:10:56.000Z',
+        'amount': '300',
+        'authoriser_contact_id': 'c4e6d5b7-a6a0-4b68-b481-0caaac0d3875',#one making payment
+        'your_bank_account_id': '8cd332a0-8cf3-479b-9e80-c17a22bf63ad'
+        }
+    url = "https://api.sandbox.split.cash/payment_requests"
+    response = requests.post(url, json.dumps(payload), headers=headers)
+    print("If successful, the response code should be = 200",response.status_code)
+
+
